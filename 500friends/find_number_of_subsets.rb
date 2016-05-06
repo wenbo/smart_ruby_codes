9e2a011c4dfa2bcf27acc3df03f04fa8e4054c33
wenbo <yiyun6674@hotmail.com>
2016-05-07 01:29:22 +0800

    now find_number_of_subsets.rb works well

---
+++ 500friends/find_number_of_subsets.rb
@@ -0,0 +1,67 @@
+TARGET_SUM = 15
+
+@stack = []
+
+@sumInStack = 0
+
+def populateSubset(data, fromIndex, endIndex)
+
+  if (@sumInStack == TARGET_SUM)
+    print_stack
+  end
+
+  for currentIndex in fromIndex..endIndex do
+    if (@sumInStack + data[currentIndex] <= TARGET_SUM)
+      @stack << data[currentIndex]
+      @sumInStack += data[currentIndex]
+
+      populateSubset(data, currentIndex + 1, endIndex)
+      @sumInStack -= @stack.pop
+    end
+  end
+
+end
+
+def print_stack
+  str = ""
+  str = str + TARGET_SUM.to_s + " = "
+  @stack.each do |i|
+    str = str + i.to_s + "+"
+  end
+  puts str[0..-2]
+end
+
+DATA = []
+15.times do |i|
+  DATA << i+1
+end
+populateSubset(DATA, 0, DATA.size-1)
+
+
+#15 = 1+2+3+4+5
+#15 = 1+2+3+9
+#15 = 1+2+4+8
+#15 = 1+2+5+7
+#15 = 1+2+12
+#15 = 1+3+4+7
+#15 = 1+3+5+6
+#15 = 1+3+11
+#15 = 1+4+10
+#15 = 1+5+9
+#15 = 1+6+8
+#15 = 1+14
+#15 = 2+3+4+6
+#15 = 2+3+10
+#15 = 2+4+9
+#15 = 2+5+8
+#15 = 2+6+7
+#15 = 2+13
+#15 = 3+4+8
+#15 = 3+5+7
+#15 = 3+12
+#15 = 4+5+6
+#15 = 4+11
+#15 = 5+10
+#15 = 6+9
+#15 = 7+8
+#15 = 15
